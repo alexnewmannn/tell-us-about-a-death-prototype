24fc32030250f45015d01d55950b249125984a3c
admin <alex.newman2@dwp.gsi.gov.uk>
2018-07-12 15:17:07 +0100

    Add search functionality, map to real results and update content changes

--- app/config.js
+++ app/config.js
@@ -25,6 +25,92 @@ module.exports = {
   cookieText: 'GOV.UK uses cookies to make the site simpler. <a href="#">Find out more about cookies</a>',
 
   // Enable or disable Browser Sync
-  useBrowserSync: 'true'
+  useBrowserSync: 'true',
 
+  searchResults: [ // These prop names aren't ideal
+    {
+      nino: 'FA119533X',
+      name: 'John Doe',
+      deceased: true,
+      dateOfDeath: '11/07/2015',
+      dateOfBirth: '08/01/1995',
+      line1: 'Benton Park View',
+      town: 'Newcastle Upon Tyne',
+      county: 'Tyne and Wear',
+      postcode: 'NE98 1ZZ',
+      notifier: {
+        name: 'Janet Doe',
+        leafletCode: 'AB1234',
+        relationshipToDeceased: 'wife',
+        line1: 'Benton Park View',
+        town: 'Newcastle Upon Tyne',
+        county: 'Tyne and Wear',
+        postcode: 'NE98 1ZZ',
+        phoneNumber: '077911234567'
+      }
+    },
+    {
+      nino: 'DA016292D',
+      name: 'Janet Doe',
+      deceased: true,
+      dateOfDeath: '27/02/2018',
+      dateOfBirth: '22/05/1996',
+      line1: 'Benton Park View',
+      town: 'Newcastle Upon Tyne',
+      county: 'Tyne and Wear',
+      postcode: 'NE98 1ZZ',
+      notifier: {
+        name: 'Jimmy Doe',
+        leafletCode: 'BC6546',
+        relationshipToDeceased: 'son',
+        line1: 'Benton Park View',
+        town: 'Newcastle Upon Tyne',
+        county: 'Tyne and Wear',
+        postcode: 'NE98 1ZZ',
+        phoneNumber: '01670123456'
+      }
+    },
+    {
+      nino: 'IB987753Y',
+      name: 'Guy Incognito',
+      deceased: true,
+      dateOfDeath: '03/04/1986',
+      dateOfBirth: '12/05/1956',
+      line1: '742 Evergreen Terrace',
+      town: 'Springfield',
+      county: 'Northumberland',
+      postcode: 'NE24 4N9',
+      notifier: {
+        name: 'Marge Incognito',
+        leafletCode: 'JH7423',
+        relationshipToDeceased: 'wife',
+        line1: '742 Evergreen Terrace',
+        town: 'Springfield',
+        county: 'Northumberland',
+        postcode: 'NE24 4N9',
+        phoneNumber: '074236734443'
+      }
+    },
+    {
+      nino: 'DA125341O',
+      name: 'Eliza Lester',
+      deceased: true,
+      dateOfDeath: '31/12/2001',
+      dateOfBirth: '19/04/1987',
+      line1: 'Benton Park View',
+      town: 'Newcastle Upon Tyne',
+      county: 'Tyne and Wear',
+      postcode: 'NE98 1ZZ',
+      notifier: {
+        name: 'Bart Incognito',
+        leafletCode: 'XX6443',
+        relationshipToDeceased: 'nurse',
+        line1: '742 Evergreen Terrace',
+        town: 'Springfield',
+        county: 'Northumberland',
+        postcode: 'NE24 4N9',
+        phoneNumber: '07423673455553'
+      }
+    }
+  ]
 }
--- app/routes.js
+++ app/routes.js
@@ -1,11 +1,25 @@
 const express = require('express')
 const router = express.Router()
+const config = require('./config.js')
+const path = require('path');
 
 // Route index page
 router.get('/', function (req, res) {
   res.render('index')
 })
 
+const search = (entry => config.searchResults.find(item => item.nino.toUpperCase() === entry.toUpperCase()));
+
+router.get('*/search-results', (req, res, next) => {
+  const query = req.session.data['search-national-insurance'];
+  res.locals.result = search(query);
+  next();
+})
+
+router.get('*/entry/*', (req, res) => {
+  res.locals.entry = search(req.params[1]);
+  res.render('versions/base/entry.html');
+})
 // Add your routes here - above the module.exports line
 
 
